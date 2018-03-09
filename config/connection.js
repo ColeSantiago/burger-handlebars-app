f4f4612dbc4d88fcf5ae7c3292f039e34288763f
Cole Santiago <colesantiago@Coles-MBP.fios-router.home>
2018-03-08 21:07:37 -0500

    added code for a heroku database

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    port: 8889,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
